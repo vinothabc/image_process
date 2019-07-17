a36cf5949a4e5144d851ef6c11b54d1e1bbdd91b
vinoth <vinoth4156@gmail.com>
2019-07-17 08:04:45 +0530

    image process --vinoth

--- image_process.js
+++ image_process.js
@@ -1,5 +1,6 @@
 //image_process.js
 var Jimp = require('jimp');
+const sharp = require('sharp');
 
 
 module.exports = {
@@ -13,12 +14,24 @@ module.exports = {
 		return save_path; 
 	},
 
-	resize_image : async function(path,width,height,save_path) {
+	resize_image1 : async function(path,width,height,save_path) {
 		const image = await Jimp.read(path); 
 		image.resize(width, height, function(err){ 
 		    if (err) throw err; 
 		}) 
 		.write(save_path);
+		return save_path; 
+	},
+	resize_image : async function(path,width,height,save_path) {
+		const image = await sharp(path)
+					  .resize(width, height, {
+					    fit: 'contain',
+					    background: { r: 255, g: 255, b: 255, alpha: 0.5 }
+					  })
+					  .toFile(save_path)
+					  .then(() => {
+							return save_path; 
+					  });
 		return save_path; 
 	}
 }
